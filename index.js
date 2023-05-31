f80cb8fd7cf1a4baff7247ba1c360187fa13e5d0
mahedi6666655555 <mahedinir34678@gmail.com>
2023-06-01 01:26:12 +0600

    part 6 is done

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obla9o6.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,17 +29,138 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db("bistroDb").collection("menu");
+
     const reviewCollection = client.db("bistroDb").collection("reviews");
 
-    app.get('/menu', async(req, res) =>{
-        const result = await menuCollection.find().toArray();
-        res.send(result);
+    const cartDb = client.db("cart").collection("Cartcollection")
+    const userSDb = client.db("usersREgistrationInfo").collection("regis")
+
+
+
+
+
+    // gere is teh users registration infos cursor 
+
+    app.get("/users", async (req, res) => {
+      const filter = userSDb.find()
+
+      const result = await filter.toArray()
+      res.send(result)
     })
-    
-    app.get('/reviews', async(req, res) =>{
-        const result = await reviewCollection.find().toArray();
-        res.send(result);
+
+
+    // here is the user register infos
+
+    app.post("/users", async (req, res) => {
+      const user = req.body
+
+      console.log(user);
+
+      const query = { email: user.email }
+
+
+
+      const existingUser = await userSDb.findOne(query);
+
+      if (existingUser) {
+        return res.send({ massage: "user already exist" })
+      }
+
+
+      const result = await userSDb.insertOne(user)
+      console.log(result);
+      res.send(result)
     })
+
+
+
+
+
+    // here is the cursor of the cart collection
+
+    app.get("/cart", async (req, res) => {
+
+
+      const email = req.query.email
+
+      console.log(email);
+
+
+
+      if (!email) {
+
+        res.send([]);
+      }
+
+      const query = { UserEmail: email };
+
+      const result = await cartDb.find(query).toArray()
+
+      res.send(result)
+
+
+    })
+
+    // here is the cursor of the cart collection
+
+
+    // here is the cart collection
+
+    app.post("/cart", async (req, res) => {
+      const cart = req.body
+
+      console.log(cart);
+
+      const result = await cartDb.insertOne(cart)
+      res.send(result)
+    })
+
+
+    // here is rhe delete system of the product
+
+    app.delete("/cart", async (req, res) => {
+      const id = req.params.id
+
+      const filter = { _id: new ObjectId(id) }
+
+      const result = await cartDb.deleteOne(filter)
+      
+      res.send(result)
+    })
+
+
+    // here is rhe delete system of the product ends
+
+
+    // here is the cart collection
+
+
+
+
+
+
+
+    app.get('/', async (req, res) => {
+    app.get('/', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post("/menu", async (req, res) => {
+      const user = req.body
+      console.log(user);
+
+    })
+
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    })
+
+
+
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -50,10 +173,16 @@ async function run() {
 run().catch(console.dir);
 
 
+
+
 app.get('/', (req, res) => {
-    res.send('boss is sitting')
+  res.send('boss is sitting')
 })
 
 app.listen(port, () => {
-    console.log(`Bistro boss is sitting on port ${port}`);
+  console.log(`Bistro boss is sitting on port ${port}`);
 })
+
+
+
+
